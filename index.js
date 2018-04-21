fc57b1fa73b47489c1b9e4a70027cbf318050d23
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2018-04-21 22:54:41 +0300

    ...

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import "./createThumbnail.js";
-import "./getPublicURL.js";
+import { createThumbnail } from "./createThumbnail.js";
+import { getPublicURL } from "./getPublicURL.js";
 
 module.exports = {
   createThumbnail: createThumbnail,
