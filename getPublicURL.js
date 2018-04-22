615f49c30d863415cb26c8f85e5ae9a3a44e0f02
Maksim Yurkov <yurkov.maksim.sergeevich@gmail.com>
2018-04-22 12:03:58 +0300

    Update getPublicURL.js

--- getPublicURL.js
+++ getPublicURL.js
@@ -1,5 +1,5 @@
 "use strict";
-module.exports = function getPublicURL() {
+module.exports = function getPublicURL(filename) {
   return `https://storage.googleapis.com/api-01-ht.appspot.com/${encodeURIComponent(
     filename
   )}`;
